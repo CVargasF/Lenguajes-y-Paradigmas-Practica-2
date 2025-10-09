a8ada9a73d2aba58e8cb042347cb194295be30bc
CVargasF <142312112+CVargasF@users.noreply.github.com>
2025-10-09 17:21:08 -0500

    Add files via upload

---
+++ Reto1/PrologPrograming.pl
@@ -0,0 +1,161 @@
+% plataforma(Marca,NumeroSerial,Año,CapRamGB,MarcaCPU,NucleosCPU,CapacidadDiscoDuro,Tipo,ManufGraf,VideoRam). 
+plataforma(hp, sn-abx425na, 2024, 32, amd, 8, 1000, computador, nvidia, 8).
+plataforma(hp, sn-uiasd234, 2023, 16, amd, 6, 512, computador, nvidia, 4).
+plataforma(hp, sn-ople2031, 2022, 8, amd, 4, 256, portatil, amd, 4).
+plataforma(hp, sn-bealtop0, 2021, 16, intel, 6, 512, computador, amd, 8).
+plataforma(hp, sn-yuinto12, 2020, 8, amd, 6, 512, computador, nvidia, 4).
+plataforma(hp, sn-quet963d, 2019, 8, intel, 6, 512, portatil, nvidia, 2).
+plataforma(hp, sn-suicpo12, 2019, 8, amd, 4, 256, portatil, amd, 2).
+plataforma(hp, sn-jnbiuyhe, 2019, 4, intel, 4, 128, tableta, amd, 4).
+plataforma(hp, sn-a1hfirq2, 2018, 8, amd, 6, 256, portatil, intel, 4).
+plataforma(hp, sn-bol231as, 2018, 8, intel, 4, 256, portatil, amd, 2).
+plataforma(hp, sn-qiout965, 2017, 4, amd, 4, 256, portatil, nvidia, 4).
+plataforma(hp, sn-ujvixuws, 2017, 4, amd, 4, 128, tableta, nvidia, 2).
+plataforma(asus, sout2-cuat, 2023, 32, amd, 8, 1000, computador, nvidia, 8).
+plataforma(asus, touw5-u213, 2022, 16, amd, 6, 512, computador, amd, 8).
+plataforma(asus, t23a-lb042, 2022, 32, amd, 8, 512, computador, amd, 4).
+plataforma(asus, plto-tn768, 2021, 32, amd, 8, 512, computador, nvidia, 4).
+plataforma(asus, hugi-xt34w, 2020, 16, intel, 6, 512, computador, intel, 4).
+plataforma(asus, ricx-g12gh, 2019, 8, intel, 4, 512, portatil, intel, 2).
+plataforma(asus, krom-m3rts, 2019, 8, amd, 4, 256, tableta, amd, 2).
+plataforma(asus, easj-pl231, 2019, 16, intel, 6, 512, computador, nvidia, 4).
+plataforma(asus, pert-cvpat, 2019, 16, intel, 6, 1000, computador, intel, 2).
+plataforma(asus, p23x-tuap7, 2018, 16, intel, 6, 512, portatil, intel, 4).
+plataforma(asus, pgha-r0974, 2017, 8, intel, 4, 256, portatil, intel, 2).
+plataforma(acer, uadaltl231pale, 2024, 64, amd, 10, 512, computador, amd, 24).
+plataforma(acer, spota231oa0pgs, 2024, 32, amd, 8, 512, portatil, nvidia, 8).
+plataforma(acer, mytba2301fmasc, 2023, 64, amd, 8, 512, computador, amd, 24).
+plataforma(acer, polti2434akfic, 2023, 32, amd, 8, 512, computador, nvidia, 8).
+plataforma(acer, gyztio2451kavc, 2022, 16, intel, 6, 512, portatil, amd, 8).
+plataforma(acer, paldaht234fjka, 2021, 32, amd, 8, 512, computador, nvidia, 8).
+plataforma(acer, pacyti29184mci, 2021, 16, amd, 6, 256, portatil, nvidia, 8).
+plataforma(acer, ydzxut2458mant, 2020, 32, amd, 8, 256, portatil, nvidia, 4).
+plataforma(acer, ugaonx2905jngt, 2019, 16, intel, 8, 512, computador, nvidia, 8).
+plataforma(acer, tuhczx3109giku, 2019, 16, intel, 6, 256, portatil, nvidia, 4).
+plataforma(lenovo, aybg23991cdc, 2024, 16, amd, 4, 512, portatil, intel, 4).
+plataforma(lenovo, uyagxb23138z, 2023, 8, intel, 4, 512, portatil, intel, 2).
+plataforma(lenovo, iuhwio12984p, 2022, 8, intel, 4, 512, computador, amd, 2).
+plataforma(lenovo, asodnh2399pa, 2022, 2, intel, 4, 128, tableta, intel, 2).
+plataforma(lenovo, tbzxuf196423, 2022, 16, intel, 6, 512, computador, intel, 2).
+plataforma(lenovo, xhutjw2o1230, 2022, 16, intel, 6, 512, computador, intel, 2).
+plataforma(lenovo, tophyts789dkg, 2020, 8, intel, 4, 256, tableta, intel, 2).
+plataforma(lenovo, tpmzxhc2341t, 2020, 8, intel, 4, 256, portatil, intel, 2).
+plataforma(lenovo, ophzoijgi58b, 2020, 8, intel, 4, 256, portatil, intel, 2).
+plataforma(lenovo, zpkhgujre30f, 2019, 16, intel, 6, 512, portatil, intel, 4).
+plataforma(lenovo, juy21347xfbs, 2019, 8, intel, 6, 512, computador, intel, 2).
+plataforma(lenovo, jh2bhuczx587, 2018, 8, intel, 4, 256, portatil, amd, 2).
+plataforma(apple, lzcv231uygfzxcvyh23, 2025, 32, apple, 8, 1000, portatil, apple, 24).
+plataforma(apple, jgnhyiua2123oazxc27, 2024, 16, apple, 8, 512, portatil, apple, 8).
+plataforma(apple, jknv6723jsytf23czau, 2024, 8, apple, 6, 512, portatil, apple, 8).
+plataforma(apple, ziuhyb234871ybvmguy, 2023, 6, apple, 4, 256, tableta, apple, 4).
+plataforma(apple, fhnjb3w54o378cnhdaj, 2023, 6, apple, 6, 256, portatil, apple, 4).
+plataforma(apple, xzyfcv1347tzaptxfvt, 2023, 4, apple, 4, 256, tableta, apple, 8).
+plataforma(microsoft, jhvbuytg324ioajnh, 2024, 2, amd, 6, 128, tableta, intel, 2).
+plataforma(microsoft, hfi2341jhbczxv23j, 2023, 16, intel, 8, 512, computador, amd, 8).
+plataforma(microsoft, ujhbvcyg31123ziuc, 2023, 8, intel, 6, 256, portatil, amd, 4).
+plataforma(microsoft, jsbnv24iu9hzt233v, 2023, 8, intel, 6, 256, portatil, amd, 8).
+plataforma(microsoft, uygzpojqr9iqu238c, 2022, 16, intel, 8, 256, portatil, amd, 4).
+%  --- Consultas básicas ---
+encontrar_numeroserial(NúmeroSerial, ListaDeDatos):-
+    findall((A,B,C,D,E,F,G,H,I), plataforma(A , NúmeroSerial, B, C, D , E, F, G, H, I), ListaDeDatos).
+longituddelista([], 0).
+longituddelista([_|B], Longitud):- longituddelista(B, LongitudCola), Longitud is 1 + LongitudCola.
+comparacionnumeral(Numero1, Numero2, Resultado):- 
+    Numero1 > Numero2, Resultado = "La primera marca tiene una cantidad mayor de plataformas especificadas. ".
+comparacionnumeral(Numero1, Numero2, Resultado):-
+    Numero1 < Numero2, Resultado = "La segunda marca tiene una cantidad mayor de plataformas especificadas. ".
+comparacionnumeral(Numero1, Numero2, Resultado):-
+	Numero1 =:= Numero2, Resultado = "Ambas marcas tienen la misma cantidad de plataformas especificadas. ".
+%  --- Consultas ---
+%Consulta 1
+encontrar_marca_añominimo(MarcaCPU, AñoMínimo, ListaDePlataformas):- 
+    findall(NúmeroSerial, 
+            (plataforma(_, NúmeroSerial, Año, _, MarcaCPU, _, _, _, _, _), Año > AñoMínimo), 
+            ListaDePlataformas).
+
+%Consulta 2
+encontrar_tipo_capacidadram(Tipo, CapacidadRamMínima, ListaDeTabletas):- 
+    findall(NúmeroSerial, 
+            (
+            plataforma(_, NúmeroSerial, _, Ram, _, _, _, Tipo, _, _),
+            Ram > CapacidadRamMínima
+            )
+            , ListaDeTabletas).
+%Consulta 3
+encontrar_tipos_con_capacidadesespecificas(CapacidadMín, CapacidadMax, NombredePlataformas):-
+    findall(NúmeroSerial, 
+            (
+            plataforma(_, NúmeroSerial, _, _, _, _, Capacidad, _, _, _)
+            , Capacidad >= CapacidadMín
+            , Capacidad =< CapacidadMax
+            ), NombredePlataformas).
+%Consulta 4
+encontrar_cantidad_de_marca(Marca, Resultado):-
+    findall(NúmeroSerial, plataforma(Marca, NúmeroSerial, _, _, _, _, _, _, _, _), 
+            ListaDePlataforma), 
+    		longituddelista(ListaDePlataforma, Resultado).
+%Consulta 5
+encontrar_cantidad_ram_disco_plataforma(Tipo, RAMMin, CapacidadMax, Resultado):-
+    findall(NúmeroSerial, 
+            (plataforma(_, NúmeroSerial, _, RAM, _, _, Capacidad, Tipo, _, _), 
+            Capacidad < CapacidadMax,
+            RAM > RAMMin), 
+            ListaDePlataforma), 
+    longituddelista(ListaDePlataforma, Resultado).
+
+
+
+
+
+
+%Consultas Adicionales
+%Consulta Ad. 1
+
+cantidad_tipo_tarjetagraf_videoram_discoduro(Marca, Tipo, NucleosCPUMin, VideoRAMMax, Resultado):-
+	findall(NúmeroSerial, 
+            (plataforma(Marca, NúmeroSerial, _, _, _, NucleosCPU, _, Tipo, _, VideoRAM), 
+            NucleosCPU > NucleosCPUMin,
+            VideoRAM < VideoRAMMax), 
+            Resultado). 
+
+%Consulta Ad. 2
+
+cantidad_tipo_tarjetagraf_videoram_discoduro(DiscoDuroMin, Tipo, TarjetaGrafica, VideoRAMMin, Resultado):-
+      findall(NúmeroSerial, 
+            (plataforma(_, NúmeroSerial, _, _, _, _, DiscoDuro, Tipo, TarjetaGrafica, VideoRAM), 
+            DiscoDuro > DiscoDuroMin,
+            VideoRAM > VideoRAMMin), 
+            Lista), 
+     longituddelista(Lista, Resultado).
+
+
+
+%Consulta Ad. 3
+
+encontrar_marcacpu_nucleosminimo_año_tipo(MínimoNucleosCPU, AñoMinimo, Tipo, Resultado):- 
+    findall(MarcaCPU,
+          (
+          plataforma(_, _, Año, _, MarcaCPU, NucleosCPU, _, Tipo, _, _), 
+          NucleosCPU > MínimoNucleosCPU, 
+          Año > AñoMinimo
+          ), 
+          Res), 
+    	  sort(Res, Resultado).
+
+%Consulta Ad. 4
+
+comparar_marcas_por_tipo_y_nucleos(Marca1, Marca2, MínimoNucleosCPU, Tipo, Resultado):-
+	findall(NúmeroSerial1, 
+            (plataforma(Marca1, NúmeroSerial1, _, _, _, CPUNucleos, _, Tipo, _, _), 
+            CPUNucleos > MínimoNucleosCPU), Lista1), 
+    findall(NúmeroSerial2, 
+            (plataforma(Marca2, NúmeroSerial2, _, _, _, CPUNucleos, _, Tipo, _, _), 
+            CPUNucleos > MínimoNucleosCPU), Lista2), 
+    longituddelista(Lista1, Num1), longituddelista(Lista2, Num2), 
+    comparacionnumeral(Num1, Num2, Resultado). 
+
+
+
+
+
+
